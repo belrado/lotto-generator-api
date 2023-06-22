6aacd520f63a33483c2fa456beecb422d44f4839
belrado <u5ink@naver.com>
2023-06-22 20:50:35 +0900

    crawling

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -44,7 +44,7 @@ export class ApiService {
       throw new NotFoundException('해당 회차는 이미 등록되어있습니다.');
     }
     //await this.batchService.lottoCrawling(drwNo.toString());
-    for (let i = 700; i < 1073; i++) {
+    for (let i = 826; i < 1073; i++) {
       await this.batchService.lottoCrawling(i.toString());
     }
   }
